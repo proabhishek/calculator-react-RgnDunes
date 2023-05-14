ed94b81e2978339f0d3dbe7e5043712a1e09e746
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-05-14 13:21:20 +0530

    calculator without css

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,53 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, {useState} from 'react';
+
+
+let butsArray = ["C", "/", "*", "-", "7", "8", "9", "+", "4", "5", "6", "1", "2", "3", "=", "0", "."]
+
+const App = () => {
+      
+    let [calculate, setCalculate] = useState("");
+
+
+
+
+    function handleCalculations(e){ 
+            setCalculate(calculate + e.target.innerText)
+    }
+
+    function result(){
+          try{
+            let ans = eval(calculate)
+            setCalculate(ans)
+          }
+          catch(err){
+             setCalculate("Error")
+          }
+
+    }
+
+    function clear(){
+        setCalculate("")
+    }
+
+    
+    return(
+        <div className="calculator">
+              <input type="text"  value={calculate}/> 
+
+              {
+                    butsArray.map((item, index) => (
+                        <button onClick={item == "=" ? result: item =="C" ? clear: handleCalculations }
+                        > {item} </button>
+                    ))
+
+                    
+              }
+              
+        </div>
+    )
+}
+
+
+export default App;
+
 
